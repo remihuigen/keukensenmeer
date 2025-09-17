b3a1250b42376197957ef0e3a12c6bba5aaaa97c
remihuigen <remihuigen@gmail.com>
2025-09-17 11:54:37 +0200

    chore: Update config files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,23 +2,23 @@
 import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt(
-  // Your custom configs here
   {
     rules: {
       'nuxt/nuxt-config-keys-order': 'off',
       'vue/multi-word-component-names': 'off',
-      '@typescript-eslint/ban-ts-comment': 'off'
-    }
+      '@typescript-eslint/ban-ts-comment': 'off',
+    },
   }
 )
   .override('nuxt/vue/rules', {
     rules: {
       'vue/no-v-html': 'error',
-      'vue/max-attributes-per-line': ['error', { singleline: 5, multiline: 1 }]
-    }
+      'vue/max-attributes-per-line': ['error', { singleline: 5, multiline: 1 }],
+    },
   })
   .override('nuxt/typescript/rules', {
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off'
-    }
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-namespace': ['error', { allowDeclarations: true }],
+    },
   })
