d994b619edf1f0d7e92aa2c5aa26902677c9bdb3
remihuigen <remihuigen@gmail.com>
2025-09-17 15:42:45 +0200

    feat: Add fonts, color and icons

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,3 +1,35 @@
 export default defineAppConfig({
-    foo: 'bar'
+    ui: {
+        colors: {
+            primary: 'primary',
+            secondary: 'secondary',
+            info: 'info',
+            success: 'success',
+            warning: 'warn',
+            error: 'error',
+            neutral: 'gray'
+        },
+        icons: {
+            light: 'heroicons:sun-16-solid',
+            dark: 'heroicons:moon-16-solid',
+            arrowLeft: 'heroicons:arrow-left',
+            arrowRight: 'heroicons:rrow-right',
+            check: 'heroicons:check',
+            chevronDoubleLeft: 'heroicons:chevron-double-left',
+            chevronDoubleRight: 'heroicons:chevron-double-right',
+            chevronDown: 'heroicons:chevron-down',
+            chevronLeft: 'heroicons:chevron-left',
+            chevronRight: 'heroicons:chevron-right',
+            chevronUp: 'heroicons:chevron-up',
+            close: 'heroicons:x-mark',
+            ellipsis: 'heroicons:ellipsis-horizontal',
+            external: 'heroicons:arrow-up-right',
+            folder: 'heroicons:folder',
+            folderOpen: 'heroicons:folder-open',
+            loading: 'heroicons:arrow-path',
+            minus: 'heroicons:minus',
+            plus: 'heroicons:plus',
+            search: 'heroicons:magnifying-glass'
+        },
+    }
 })
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,7 +3,14 @@
 </script>
 
 <template>
-  <div>
-    <h1>My index page</h1>
-  </div>
+  <NuxtLayout>
+    <UContainer class="py-8">
+      <h1 class="font-bold font-serif text-4xl text-white">Maatwerk keukens die het ritme van <span
+          class="font-hand text-primary-500 text-8xl ml-2.5 sm:ml-1.5 whitespace-nowrap leading-[0]">jouw
+          leven</span> volgen. </h1>
+      <p>This is the content of the index page.</p>
+      <UButton to="/page" label="Other page" />
+    </UContainer>
+
+  </NuxtLayout>
 </template>
