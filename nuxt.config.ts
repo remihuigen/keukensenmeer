fe54258603937cda9eadac462513ddd879b88fc6
Remi Huigen <remihuigen@proton.me>
2025-12-12 13:21:34 +0100

    chore: Update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ import projects from './data/projects'
 
 const isDebug = process.env.DEBUG === 'true' || false
 const isDev = process.env.MODE === 'dev'
+const isPreview = process.env.MODE === 'preview'
 const isProd = process.env.MODE === 'production'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
@@ -26,7 +27,7 @@ export default defineNuxtConfig({
   css: ['~/assets/css/main.css'],
 
   nitro: {
-    minify: process.env.DEBUG !== 'true',
+    minify: !isDebug,
     prerender: {
       crawlLinks: true,
       failOnError: false,
@@ -35,8 +36,49 @@ export default defineNuxtConfig({
       openAPI: true
     }
   },
-  debug: isDebug,
+  // List of debug options for various Nuxt subsystems
+  debug: {
+    nitro: isDebug,
+    hydration: isDebug || isDev || isPreview,
+    watchers: isDebug || isDev,
+    router: isDebug,
+    templates: isDebug,
+    modules: isDebug,
+    hooks: {
+      server: isDebug,
+      client: isDebug,
+    },
+  },
+
   ssr: true,
+
+  // Disable caching in development to prevent confusion
+  $development: {
+    routeRules: {
+      '/**': { cache: false },
+    },
+  },
+
+  routeRules: {
+    '/': {
+      ssr: true,
+      cache: {
+        maxAge: 60 * 60 * 24
+      }
+    },
+    '/**': {
+      ssr: true,
+      cache: {
+        maxAge: 60 * 60 * 24
+      }
+    },
+    '/projecten/**': {
+      ssr: true,
+      cache: {
+        maxAge: 60 * 60 * 24
+      }
+    },
+  },
 
 
   runtimeConfig: {
